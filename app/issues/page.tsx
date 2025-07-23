e2635deba5a9e277d357d738341b708a1610034c
Gowrishankar Mukkeri <gowrishankar.mukkeri@external.minna.tech>
2025-07-23 18:40:54 +0530

    feat: setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const Issues = () => {
-  return <div>Issues</div>;
+  return (
+    <div>
+      <Button>Issue new</Button>
+    </div>
+  );
 };
 
 export default Issues;
