89ac4bec8a563815a784ea92a1e3d5cd4714ca50
Gowrishankar Mukkeri <gowrishankar.mukkeri@external.minna.tech>
2025-07-23 23:02:28 +0530

    feat: handled error sent from server

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 
 // Dynamically import SimpleMDE editor with SSR disabled to avoid 'navigator is not defined' error
@@ -19,24 +20,40 @@ interface IssueForm {
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-4"
-      onSubmit={handleSubmit(async ({ title, description }) => {
-        await axios.post("/api/issues", { title, description });
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root {...register("title")}>
-        <TextField.Slot>Title</TextField.Slot>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => <SimpleMDE {...field} />}
-      />
-      <Button type="submit">Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root
+          className="mb-5"
+          color="red"
+          style={{ minHeight: "2.5rem" }}
+        >
+          <Callout.Text>{error || "\u00A0"}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-4"
+        onSubmit={handleSubmit(async ({ title, description }) => {
+          try {
+            await axios.post("/api/issues", { title, description });
+            router.push("/issues");
+          } catch (error) {
+            setError("Failed to create issue");
+          }
+        })}
+      >
+        <TextField.Root {...register("title")}>
+          <TextField.Slot>Title</TextField.Slot>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE {...field} />}
+        />
+        <Button type="submit">Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
