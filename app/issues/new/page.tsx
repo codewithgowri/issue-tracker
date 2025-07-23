a828b279033b82152664e5cd2baa3e3f2adf793d
Gowrishankar Mukkeri <gowrishankar.mukkeri@external.minna.tech>
2025-07-23 21:16:53 +0530

    feat: desgined ui for creating issue and integrated api

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,42 @@
+"use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useForm, Controller } from "react-hook-form";
+
+// Dynamically import SimpleMDE editor with SSR disabled to avoid 'navigator is not defined' error
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
   return (
-    <div className="max-w-xl space-y-4">
-      <TextField.Root>
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit(async ({ title, description }) => {
+        await axios.post("/api/issues", { title, description });
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root {...register("title")}>
         <TextField.Slot>Title</TextField.Slot>
       </TextField.Root>
-      <TextArea placeholder="Description" />
-      <Button>Submit New Issue</Button>{" "}
-    </div>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE {...field} />}
+      />
+      <Button type="submit">Submit New Issue</Button>
+    </form>
   );
 };
 
