790583724c10df6572c6f2cd38fd8a303019cff7
Gowrishankar Mukkeri <gowrishankar.mukkeri@external.minna.tech>
2025-07-23 15:08:38 +0530

    feat: build an api for creating issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const issueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255, "Title must be less than 255 characters"),
+  description: z.string().min(1, "Description is required"),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(
+      {
+        errors: validation.error.issues,
+      },
+      { status: 400 }
+    );
+  }
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: validation.data.title,
+      description: validation.data.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
