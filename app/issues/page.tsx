ce1c090d3a2b076c349b1fbc5cc5384bb42ea285
Gowrishankar Mukkeri <gowrishankar.mukkeri@external.minna.tech>
2025-07-24 23:15:25 +0530

    feat: build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import prisma from "@/lib/prisma";
 import { Table } from "@radix-ui/themes";
-import Link from "next/link";
 import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
+import Link from "../components/Link";
 
 const Issues = async () => {
   const issues = await prisma.issue.findMany();
