d03f984d7595ed91fd06f705d3bfef38dc0d932b
Gowrishankar Mukkeri <gowrishankar.mukkeri@external.minna.tech>
2025-08-12 14:19:55 +0530

    feat: created a seperate file for authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+import authOptions from "@/app/auth/authOptions";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-};
-const handler = NextAuth({
-  ...authOptions,
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
